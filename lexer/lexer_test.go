6dc64c449f68ffa8f41d05262c07b8612a4883e7
Obsidian <ObsidianRock@users.noreply.github.com>
2017-11-10 22:24:25 +0000

    added lexer test

---
+++ lexer/lexer_test.go
@@ -0,0 +1,42 @@
+package lexer
+
+
+import (
+  "testing"
+  "gipx/token"
+)
+
+func TestNextToken(t *testing.T){
+
+  input := `=+(){},;`
+
+  test :=[]struct{
+    expectedType token.TokenType
+    expectedLiteral string
+  }{
+    {token.ASSIGN,"="},
+    {token.PLUS,"+"},
+    {token.LPARAN,"("},
+    {token.RPARAN,")"},
+    {token.LBRACE,"{"},
+    {token.RBRACE,"{"},
+    {token.COMMA,","},
+    {token.SEMICOLON,";"},
+    {token.EOF,""},
+  }
+
+  l := New(input)
+
+  for i, tt := range tests {
+
+    tok := l.NextToken()
+
+    if tok.Type != tt.expectedType {
+      t.Fatalf("test[%d] - tokenType wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+    }
+
+    if tok.Literal != tt.expectedLiteral {
+      t.Fatalf("test[%d] - tokenLiteral wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+    }
+  }
+}
