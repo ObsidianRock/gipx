91a422247ae9e1f8a060474047e01035b3a778bc
Obsidian <ObsidianRock@users.noreply.github.com>
2017-11-11 22:29:27 +0000

    parser,parseLetStatement

--- parser/parser.go
+++ parser/parser.go
@@ -19,13 +19,13 @@ func New(l *lexer.Lexer) *Parser {
   p := &Parser{l: l}
 
   // Read two tokens, so curToken and peekToken are both set
-  p.NextToken()
-  p.NextToken()
+  p.nextToken()
+  p.nextToken()
 
   return p
 }
 
-func (p *Parser) NextToken() {
+func (p *Parser) nextToken() {
 
   p.curToken = p.peekToken
   p.peekToken = p.l.NextToken()
@@ -41,17 +41,54 @@ func (p *Parser) ParseProgram() *ast.Program {
     if stmt != nil {
       program.Statements = append(program.Statements, stmt)
     }
-    p.NextToken()
+    p.nextToken()
   }
 
   return program
 }
 
-func (p *Parser) ParseStatement() ast.Statement {
+func (p *Parser) parseStatement() ast.Statement {
   switch p.curToken.Type {
   case token.LET:
     return p.parseLetStatement()
   default:
     return nil
+  }
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+
+  stmt := &ast.LetStatement{Token: p.curToken}
+  if !p.expectPeek(token.IDENT) {
+    return nil
+  }
+
+  stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+  if !p.expectPeek(token.ASSIGN) {
+    return nil
+  }
+
+  for !p.curTokenIs(token.SEMICOLON) {
+    p.nextToken()
+  }
+
+  return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+  return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+  return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+  if p.peekTokenIs(t) {
+    p.nextToken()
+    return true
+  } else {
+    return false
   }
 }
