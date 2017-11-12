99b365d4e06cb59b815964e991ec8341f5a0873c
Obsidian <ObsidianRock@users.noreply.github.com>
2017-11-12 18:25:49 +0000

    define returnstatment + tests

--- ast/ast.go
+++ ast/ast.go
@@ -53,3 +53,12 @@ func (p *Program) TokenLiteral() string{
     return ""
   }
 }
+
+
+type ReturnStatement struct {
+  Token token.Token
+  ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
